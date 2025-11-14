feaf4b07943100c4afdb52c403b43e4b64720eae
Sunil Patel <126328736+spatelsuy@users.noreply.github.com>
2025-11-13 19:52:31 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import dbRoutes from './dbRoutes.js';
+import aiRoutes from './aiRoutes.js';
 
 const router = express.Router();
 
 // Use different route files
 router.use('/db', dbRoutes);        // /api/testdb
+router.use('/ai', aiRoutes);
 //router.use('/', promptRoutes);        // /api/testdb
 
 export default router;
